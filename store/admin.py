d22477be12e53ab7e224e7894dc68a254d2cf7f7
BoastfulApex <jahongirnormuminov8@gmail.com>
2023-10-10 13:29:34 +0500

    admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Product)
+admin.site.register(News)
+admin.site.register(FAQ)
+admin.site.register(About)
