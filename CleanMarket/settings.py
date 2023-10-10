d4c20adbb90cd98a393e3205c1ce9a577375d414
BoastfulApex <jahongirnormuminov8@gmail.com>
2023-10-10 10:48:34 +0500

    fixed

--- CleanMarket/settings.py
+++ CleanMarket/settings.py
@@ -124,11 +124,11 @@ import os
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/files/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'files/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
